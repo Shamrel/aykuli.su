769774de85e3b4ecfecbae5f463287a20e49340b
aynur <aykuli@ya.ru>
2023-01-05 12:02:06 +0700

    prodcution hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  host = 'aykuli.su'
+  config.hosts += [host, "http://#{host}", "https://#{host}", "http://www.#{host}", "https://www.#{host}"]  # Additional comma-separated hosts for development.
 
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
